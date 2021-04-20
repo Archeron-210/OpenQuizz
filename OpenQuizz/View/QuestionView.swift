cce39598d4f20d11f1170c2128dbfb9b2aacbe49
Laura Jacquin <laura.jcqn@live.fr>
2021-04-20 16:45:05 +0200

    Adding subclass QuestionView to UIView, connecting properties to label and icon

---
+++ OpenQuizz/View/QuestionView.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionView.swift
+//  OpenQuizz
+//
+//  Created by Archeron on 20/04/2021.
+//
+
+import UIKit
+
+class QuestionView: UIView {
+
+    @IBOutlet private var label: UILabel!
+    @IBOutlet private var icon: UIImageView!
+
+}
