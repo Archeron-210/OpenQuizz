806d2d2d396da99f42e5050fc83a2856888c7828
Laura Jacquin <laura.jcqn@live.fr>
2021-04-21 15:24:28 +0200

    Adding properties and functions in QuestionView

--- OpenQuizz/View/QuestionView.swift
+++ OpenQuizz/View/QuestionView.swift
@@ -8,8 +8,39 @@
 import UIKit
 
 class QuestionView: UIView {
+    enum Style {
+        case correct, incorrect, standard
+    }
+    
+    var style: Style = .standard {
+        didSet {
+            setStyle(style)
+        }
+    }
+
+    private func setStyle(_ style: Style) {
+        switch style {
+        case .correct:
+            backgroundColor = UIColor(red: 200.0/255.0, green: 236.0/255, blue: 148.0/255, alpha: 1)
+            icon.image = #imageLiteral(resourceName: "Icon Correct")
+            icon.isHidden = false
+        case .incorrect:
+            backgroundColor = #colorLiteral(red: 0.9522046447, green: 0.5272625685, blue: 0.5785809159, alpha: 1)
+            icon.image = #imageLiteral(resourceName: "Icon Error")
+            icon.isHidden = false
+        case . standard:
+            backgroundColor = #colorLiteral(red: 0.7497624755, green: 0.7689997554, blue: 0.7860534191, alpha: 1)
+            icon.isHidden = true
+        }
+    }
+    
+    var title = "" {
+        didSet {
+            label.text = title
+        }
+    }
 
     @IBOutlet private var label: UILabel!
     @IBOutlet private var icon: UIImageView!
-
+    
 }
